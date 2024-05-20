b685a68c27e51fd10c1a2011107f3e255da2b326
Srinath Bindumadhavan <bsrinath.65@gmail.com>
2024-05-20 19:12:02 +0530

    add player movement

---
+++ actions.py
@@ -0,0 +1,13 @@
+class Action:
+    pass
+
+
+class EscapeAction(Action):
+    pass
+
+class MovementAction(Action):
+    def __init__(self , dx:int , dy:int) -> None:
+        super().__init__()
+
+        self.dx = dx
+        self.dy = dy
